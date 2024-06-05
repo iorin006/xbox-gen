444a47a3e2610e6d965f5a09ff396d922b626f54
iorin006 <108205818+iorin006@users.noreply.github.com>
2024-06-05 21:25:30 +0900

    Add files via upload

--- xbox.py
+++ xbox.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 import random, string, threading
-import time, os
+import os
 os.system('color 0A')
 user_home = os.path.expanduser("~")
 client_id_path = os.path.join(
@@ -39,26 +39,26 @@ def gen():
     browser.get(
         "https://signup.live.com/signup?wa=wsignin1.0&rpsnv=13&rver=7.3.6963.0&wp=MBI_SSL&wreply=https:%2f%2faccount.xbox.com%2fja-jp%2faccountcreation%3frtc%3d1%26csrf%3diRRDbBsXHWOzqJoxX9GqJOfUcAQCvJVJSNVhpu9YR0ntJtPfRjwCMjSg4qE1UQC4yx6KIvX4cVItbVhM5kW-6bAyA7o1&id=292543&aadredir=1&contextid=8369C2F0524F361B&bk=1602012918&uiflavor=web&lic=1&mkt=ja-jp&lc=1033&uaid=3ba71ae4427e4c300da204fc26106240"
     )
-    time.sleep(1)
     browser.implicitly_wait(20)
     browser.find_element(By.ID, "liveSwitch").click()
-    browser.find_element(By.ID, "usernameInput").send_keys("a" + w)
-    browser.find_element(By.ID, "nextButton").click()
+    browser.implicitly_wait(20)
+    browser.find_element(By.ID, "MemberName").send_keys("a" + w)
+    browser.find_element(By.ID, "iSignupAction").click()
     browser.implicitly_wait(20)
     p = randomname(10)
-    browser.find_element(By.ID, "Password").send_keys(p)
-    browser.find_element(By.ID, "nextButton").click()
+    browser.find_element(By.ID, "PasswordInput").send_keys(p)
+    browser.find_element(By.ID, "iSignupAction").click()
     f = open(f"{name}pack.txt", "a")
     f.write(f"a{w}@outlook.jp\n{p}\n")
     browser.implicitly_wait(20)
-    browser.find_element(By.ID, "lastNameInput").send_keys("田中")
-    browser.find_element(By.ID, "firstNameInput").send_keys("太郎")
-    browser.find_element(By.ID, "nextButton").click()
+    browser.find_element(By.ID, "LastName").send_keys("1")
+    browser.find_element(By.ID, "FirstName").send_keys("1")
+    browser.find_element(By.ID, "iSignupAction").click()
     browser.implicitly_wait(60)
     browser.find_element(By.ID, "BirthYear").send_keys("1987")
     browser.find_element(By.ID, "BirthMonth").send_keys("1")
     browser.find_element(By.ID, "BirthDay").send_keys("1")
-    browser.find_element(By.ID, "nextButton").click()
+    browser.find_element(By.ID, "iSignupAction").send_keys(Keys.RETURN)
     browser.implicitly_wait(60000)
     browser.find_element(By.ID, "declineButton").click()
     browser.implicitly_wait(60)
